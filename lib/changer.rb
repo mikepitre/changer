135ea3a0cc5e111ccdff967df977e9943383802e
Mike Pitre <michaelepitre@gmail.com>
2015-06-29 09:51:52 -0500

    changer with TDD passing

--- lib/changer.rb
+++ lib/changer.rb
@@ -1,5 +1,41 @@
 class Changer
   def self.run(amt)
+    @coins = []
+    @amt = amt
+    quarters(@amt)
+    dimes(@amt)
+    nickels(@amt)
+    pennies(@amt)
 
+    @coins
   end
+
+  def self.quarters(amt)
+    (@amt/25).times do
+      @coins << 25
+      @amt -= 25
+    end
+  end
+
+  def self.dimes(amt)
+    (@amt/10).times do
+      @coins << 10
+      @amt -=10
+    end
+  end
+
+  def self.nickels(amt)
+    (@amt/5).times do
+      @coins << 5
+      @amt -= 5
+    end
+  end
+
+  def self.pennies(amt)
+    (@amt/1).times do
+      @coins << 1
+      @amt -= 1
+    end
+  end
+
 end
--- spec/changer_test.rb
+++ spec/changer_test.rb
@@ -12,4 +12,29 @@ class ChangerTest < MiniTest::Test
     assert_respond_to(Changer, :run)
   end
 
+  def test_empty_array_created
+    assert_equal([], Changer.run(0))
+  end
+
+  def test_quarter_returns_something
+    assert_equal([25,25,25], Changer.run(75))
+  end
+
+
+  def test_dimes_works
+    assert_equal([10, 10], Changer.run(20))
+  end
+
+  def test_nickels_works
+    assert_equal([10, 5], Changer.run(15))
+  end
+
+  def test_penny_works
+    assert_equal([1, 1, 1, 1], Changer.run(4))
+  end
+
+  def test_case_72_cents
+    assert_equal([25, 25, 10, 10, 1, 1], Changer.run(72))
+  end
+
 end
